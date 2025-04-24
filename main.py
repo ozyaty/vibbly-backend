49ebd018b937298f56d0b2c7f6c97cecb77ddca1
ozyaty <78293001+ozyaty@users.noreply.github.com>
2025-04-24 14:02:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from api.routes import router
+import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Welcome to Telegram Social App"}
-
 app.include_router(router)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to Vibbly Backend!"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
